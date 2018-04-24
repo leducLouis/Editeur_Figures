48a22a537bcf0cc978b944e47f365a27c0d8bec5
Louis Leduc <lleduc@emi.u-bordeaux1.fr>
2018-04-24 15:05:48 +0200

    ajouts

--- src/application/Shape.java
+++ src/application/Shape.java
@@ -1,5 +1,9 @@
 package application;
 
 public interface Shape {
-
+	void translation();
+	void addEntity();
+	void removeEntity();
+	//iterator<Entity> getChildren();
+	
 }
---
+++ src/application/ShapeComposant.java
@@ -0,0 +1,7 @@
+package application;
+
+public interface ShapeComposant {
+	void addEntity();
+	void removeEntity();
+	void getChildren();
+}
