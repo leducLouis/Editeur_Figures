bb6135a9fac91c95fe6cce07df45e3df93553e7c
Redboylevrai <paullhsimorre@gmail.com>
2018-04-27 05:51:54 +0200

    corrections

--- src/application/ControllerJavaFx.java
+++ src/application/ControllerJavaFx.java
@@ -1,6 +1,5 @@
 package application;
 
-<<<<<<< HEAD
 import java.awt.Color;
 import java.util.ArrayList;
 import model.ShapeComposant;
@@ -11,13 +10,6 @@ public class ControllerJavaFx implements Controller{
 	private ArrayList<ShapeComposant> toolBarShapes;
 	private ArrayList<ShapeComposant> canvasShapes;
 	
-=======
-import view.Fenetre;
-
-public class ControllerJavaFx implements Controller{
-
-	private Fenetre fenetre ;
->>>>>>> 4a5eea24373fdd703ade382870bb3a9bcc40ad91
 	public ControllerJavaFx() {
 		toolBarShapes.add(new ShapeRectangle(5.0,5.0,25,10,0.0,new Color(10,220,30),0));
 	}
