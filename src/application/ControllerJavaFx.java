5521d95fd02d89e6df306ac5418a78a4eee1bff1
Redboylevrai <paullhsimorre@gmail.com>
2018-04-27 05:47:28 +0200

    draganddrop debut

--- src/application/ControllerJavaFx.java
+++ src/application/ControllerJavaFx.java
@@ -1,9 +1,17 @@
 package application;
 
-public class ControllerJavaFx implements Controller{
+import java.awt.Color;
+import java.util.ArrayList;
+import model.ShapeComposant;
+import model.ShapeRectangle;
 
+public class ControllerJavaFx implements Controller{
+	
+	private ArrayList<ShapeComposant> toolBarShapes;
+	private ArrayList<ShapeComposant> canvasShapes;
+	
 	public ControllerJavaFx() {
-		// TODO Auto-generated constructor stub
+		toolBarShapes.add(new ShapeRectangle(5.0,5.0,25,10,0.0,new Color(10,220,30),0));
 	}
 
 	@Override
