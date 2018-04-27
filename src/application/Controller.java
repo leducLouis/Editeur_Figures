4e5331e5a5976bcd10beeb4b1e8d0e073478cfa5
Louis Leduc <leduclouis64@hotmail.fr>
2018-04-27 04:05:22 +0200

    ajout du controller

---
+++ src/application/Controller.java
@@ -0,0 +1,9 @@
+package application;
+
+import view.Fenetre;
+
+public interface Controller {
+	public void createView();
+	public void updateView();
+	
+}
---
+++ src/application/ControllerJavaFx.java
@@ -0,0 +1,21 @@
+package application;
+
+public class ControllerJavaFx implements Controller{
+
+	public ControllerJavaFx() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
